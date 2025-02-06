e63ad54e34b4c12393558f646525bcae46138c30
miltim7 <timursailing70@gmail.com>
2025-02-06 15:38:21 +0400

    new website-landing

---
+++ send_message.php
@@ -0,0 +1,37 @@
+<?php
+$to = "timursailing70@gmail.com";
+
+if (!empty($_POST['honeypot'])) {
+    exit;
+}
+
+function sanitize_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
+    return $data;
+}
+
+$name = isset($_POST['name']) ? sanitize_input($_POST['name']) : '';
+$email = isset($_POST['email']) ? sanitize_input($_POST['email']) : '';
+$phone = isset($_POST['phone']) ? sanitize_input($_POST['phone']) : '';
+$message = isset($_POST['message']) ? sanitize_input($_POST['message']) : '';
+
+$body = "
+Имя: $name
+Email: $email
+Номер телефона: $phone
+Сообщение: $message
+";
+
+$headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+$subject = "Новое сообщение из сайта Aagroup";
+
+if (mail($to, $subject, $body, $headers)) {
+    header("Location: /");
+    exit;
+} else {
+    echo "При отправке сообщения возникли ошибки.";
+}
+?>
